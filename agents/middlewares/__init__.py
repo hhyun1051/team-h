c6248aad0f397d4def80ece0b90ce22403d2a1b1
hhyun <hhyun1051@gmail.com>
2025-11-22 09:14:00 +0000

    refactor: Remove agent middleware defaults file.

--- agents/middlewares/__init__.py
+++ agents/middlewares/__init__.py
@@ -8,14 +8,18 @@ Agent Middlewares Package
 
 Usage:
     from agents.middlewares import LangfuseToolLoggingMiddleware, ToolErrorHandlerMiddleware
+
+    # 직접 조합하여 사용
+    middlewares = [
+        LangfuseToolLoggingMiddleware(verbose=True),
+        ToolErrorHandlerMiddleware()
+    ]
 """
 
 from .langfuse_logging import LangfuseToolLoggingMiddleware
 from .error_handler import ToolErrorHandlerMiddleware
-from .defaults import DefaultMiddlewares
 
 __all__ = [
     "LangfuseToolLoggingMiddleware",
     "ToolErrorHandlerMiddleware",
-    "DefaultMiddlewares",
 ]
